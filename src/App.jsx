9c4a717a072474170741cdf83c54886128d5e39a
Abhijithkrishna2001 <abhijithkrishna644@gmail.com>
2025-05-30 10:25:03 +0530

    change the vite.config, package.json, index.html, App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 
-import React from "react"
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
-import { Homes } from "./Pages/Home"
-import { Toaster } from "@/Components/ui/toaster"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Homes } from "./Pages/Home";
+import { Toaster } from "@/Components/ui/toaster";
+// import NotFount from "./Pages/NotFount";
+
 function App() {
   return (
-    <>
-     <Toaster/>
-      <Router basename="/my-portfolio" >
-        <Routes>
-          <Route index element={<Homes />} />
-        </Routes>
-      </Router>
-    </>
-  )
+    <BrowserRouter basename="/my-portfolio">
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Homes />} />
+        {/* <Route path="*" element={<NotFount />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
